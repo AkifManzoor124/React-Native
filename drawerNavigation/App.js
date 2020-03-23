b71055811664299c9ca497aefa18af23f35039d5
Akif Manzoor <akifmanzoor124@gmail.com>
2020-03-22 23:38:41 -0400

    used the example given in react navigation website Seperated the code into different files

---
+++ drawerNavigation/App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+
+import DrawerNavigation from './navigation/DrawerNavigation'
+
+export default function App(){
+  return <DrawerNavigation/>
+}
